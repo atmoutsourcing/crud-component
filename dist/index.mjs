fbb536d398cd10e32408bd91635bb09373672644
Bruno Fabre <bruno.hfabre@gmail.com>
2023-07-06 11:50:13 -0300

    add build command

---
+++ dist/index.mjs
@@ -0,0 +1,8 @@
+// src/CrudComponent.tsx
+import { jsx } from "react/jsx-runtime";
+function CrudComponent() {
+  return /* @__PURE__ */ jsx("div", { children: "crud component" });
+}
+export {
+  CrudComponent
+};
